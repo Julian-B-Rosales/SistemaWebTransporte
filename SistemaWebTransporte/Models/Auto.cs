5ca672924aa52fbcb24ad377c34bf8d930030666
Federico Fantacone <fede26f@hotmail.com>
2022-11-29 17:37:54 -0300

    Agregar archivos de proyecto.

---
+++ SistemaWebTransporte/Models/Auto.cs
@@ -0,0 +1,29 @@
+﻿using SistemaWebTransporte.Validations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SistemaWebTransporte.Models
+{
+    [Table("Auto")]
+    public class Auto
+    {
+        
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Modelo { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        [CheckValidYear]
+        public int Anio { get; set; }
+        [Required]
+        public string Color { get; set; }
+
+    }
+}
